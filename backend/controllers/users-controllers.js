185cacdbec28cfb40bfadb4b24d6d82f62f5ea63
Wei-Chin, Chien <r08921a26@ntu.edu.tw>
2021-01-06 01:26:06 +0800

    add jwt token

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -1,8 +1,9 @@
 const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const HttpError = require("../models/http-error");
 const User = require("../models/user");
-const bcrypt = require("bcryptjs");
 
 const getUsers = async (req, res, next) => {
     let users;
@@ -68,7 +69,7 @@ const signup = async (req, res, next) => {
     try {
         await createdUser.save();
     } catch (err) {
-        console.log(err);
+        // console.log(err);
         const error = new HttpError(
             "Signing up failed, please try again later.",
             500
@@ -76,7 +77,26 @@ const signup = async (req, res, next) => {
         return next(error);
     }
 
-    res.status(201).json({ user: createdUser.toObject({ getters: true }) });
+    let token;
+    try {
+        token = jwt.sign(
+            { userId: createdUser.id, email: createdUser.email },
+            "secret_key",
+            { expiresIn: "1h" }
+        );
+    } catch (error) {
+        const err = new HttpError(
+            "Signing up failed, please try again later.",
+            500
+        );
+        return next(err);
+    }
+
+    res.status(201).json({
+        userId: createdUser.id,
+        email: createdUser.email,
+        token: token,
+    });
 };
 
 const login = async (req, res, next) => {
@@ -119,9 +139,25 @@ const login = async (req, res, next) => {
         return next(error);
     }
 
+    let token;
+    try {
+        token = jwt.sign(
+            { userId: createdUser.id, email: createdUser.email },
+            "secret_key",
+            { expiresIn: "1h" }
+        );
+    } catch (error) {
+        const err = new HttpError(
+            "Signing up failed, please try again later.",
+            500
+        );
+        return next(err);
+    }
+
     res.json({
-        message: "Logged in!",
-        user: existingUser.toObject({ getters: true }),
+        userId: existingUser.id,
+        email: existingUser.email,
+        token: token,
     });
 };
 
