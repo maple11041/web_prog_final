cc01829be4ca5572f08fc9c18803ddadd4dc99e5
Wei-Chin, Chien <r08921a26@ntu.edu.tw>
2021-01-07 20:10:39 +0800

    add create groups

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const usersRoutes = require("./routes/users-routes");
+const groupRoutes = require("./routes/groups-routes");
 const HttpError = require("./models/http-error");
 
 const app = express();
@@ -27,6 +28,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/users", usersRoutes);
+app.use("/api/groups", groupRoutes);
 
 app.use((req, res, next) => {
     const error = new HttpError("Could not find this route.", 404);
