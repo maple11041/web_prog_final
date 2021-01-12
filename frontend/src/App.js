006f4f4bc582786e9eb6f4ccf685ee3a3b058a6b
Wei-Chin, Chien <r08921a26@ntu.edu.tw>
2021-01-12 23:00:40 +0800

    modify shop

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,29 @@ import NavBar from "./components/NavBar";
 import Shops from "./components/Shops";
 
 function App() {
+    const shopData = {
+        title: "飲料店總覽",
+        body: [
+            {
+                title: "50嵐",
+                text: "飲料店",
+                img:
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgdsakNc9_icV4v2aGsPcTiuRSlU1Ya2UQ5g&usqp=CAU",
+            },
+            {
+                title: "可不可",
+                text: "飲料店",
+                img:
+                    "https://www.boncity.com/s/img/infos/51790_1.jpg?20191014082537",
+            },
+            {
+                title: "茶湯會",
+                text: "飲料店",
+                img:
+                    "https://www.boncity.com/s/img/infos/51790_1.jpg?20191014082537",
+            },
+        ],
+    };
     return (
         <Router>
             <div className="App">
@@ -17,7 +40,10 @@ function App() {
                     <Route exact path="/" component={Login} />
                     <Route path="/sign-in" component={Login} />
                     <Route path="/sign-up" component={SignUp} />
-                    <Route path="/shop" component={Shops} />
+                    <Route
+                        path="/shop"
+                        component={() => <Shops shops={shopData} />}
+                    />
                 </Switch>
             </div>
         </Router>
--- frontend/src/components/Shops.js
+++ frontend/src/components/Shops.js
@@ -16,64 +16,32 @@ import {
 } from "reactstrap";
 import "./Shop.css";
 
-const Shops = () => {
+const Shops = ({ shops }) => {
     const [modal, setModal] = useState(false);
     const toggle = () => setModal(!modal);
+    // console.log(shops.body);
+    const render = shops.body.map((shop) => {
+        return (
+            <Col xs="12" md="4" lg="3">
+                <Card>
+                    <CardImg top width="30px" height="200px" src={shop.img} />
+                    <CardBody>
+                        <CardTitle tag="h2">{shop.title}</CardTitle>
+                        <CardText>{shop.text}</CardText>
+                        <Button>查看菜單</Button>
+                        <Button onClick={toggle}>我要開團</Button>
+                    </CardBody>
+                </Card>
+            </Col>
+        );
+    });
+    // console.log(render);
 
     return (
         <div className="shop-wrapper">
             <div className="shop-container">
-                <h2>飲料店總覽</h2>
-                <Row>
-                    <Col xs="12" md="4" lg="3">
-                        <Card>
-                            <CardImg
-                                top
-                                width="30px"
-                                height="200px"
-                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgdsakNc9_icV4v2aGsPcTiuRSlU1Ya2UQ5g&usqp=CAU"
-                            />
-                            <CardBody>
-                                <CardTitle tag="h2">50嵐</CardTitle>
-                                <CardText>飲料店</CardText>
-                                <Button>查看菜單</Button>
-                                <Button onClick={toggle}>我要開團</Button>
-                            </CardBody>
-                        </Card>
-                    </Col>
-                    <Col xs="12" md="4" lg="3">
-                        <Card>
-                            <CardImg
-                                top
-                                width="30px"
-                                height="200px"
-                                src="https://www.boncity.com/s/img/infos/51790_1.jpg?20191014082537"
-                            />
-                            <CardBody>
-                                <CardTitle tag="h2">可不可</CardTitle>
-                                <CardText>飲料</CardText>
-                                <Button>查看菜單</Button>
-                                <Button>我要開團</Button>
-                            </CardBody>
-                        </Card>
-                    </Col>
-                    <Col xs="12" md="4" lg="3">
-                        <Card>
-                            <CardImg
-                                top
-                                width="30px"
-                                height="200px"
-                                src="https://www.boncity.com/s/img/infos/51790_1.jpg?20191014082537"
-                            />
-                            <CardBody>
-                                <CardTitle tag="h2">可不可</CardTitle>
-                                <CardText>飲料</CardText>
-                                <Button>查看菜單</Button>
-                                <Button>我要開團</Button>
-                            </CardBody>
-                        </Card>
-                    </Col>
-                </Row>
+                <h2>{shops.title}</h2>
+                <Row>{render}</Row>
                 <Modal isOpen={modal} toggle={toggle}>
                     <ModalHeader>50嵐</ModalHeader>
                     <ModalBody>
