7386ac4c725ab6fdac76e1f8d78f5b303f3fc950
Wei-Chin, Chien <r08921a26@ntu.edu.tw>
2021-01-16 22:39:12 +0800

    add sidebar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,6 @@ import { React, useState } from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import SideNav, {
-    Toggle,
-    Nav,
-    NavItem,
-    NavIcon,
-    NavText,
-} from "@trendmicro/react-sidenav";
 
 // Be sure to include styles at some point, probably during your bootstraping
 import "@trendmicro/react-sidenav/dist/react-sidenav.css";
@@ -49,41 +42,8 @@ function App() {
     };
     return (
         <Router>
+            <Sidebar />
             <div className="App">
-                <Route
-                    render={({ location, history }) => (
-                        <SideNav
-                            onSelect={(selected) => {
-                                const to = "/" + selected;
-                                if (location.pathname !== to) {
-                                    history.push(to);
-                                }
-                            }}
-                        >
-                            <SideNav.Toggle />
-                            <SideNav.Nav defaultSelected="home">
-                                <NavItem eventKey="home">
-                                    <NavIcon>
-                                        <i
-                                            className="fa fa-fw fa-home"
-                                            style={{ fontSize: "1.75em" }}
-                                        />
-                                    </NavIcon>
-                                    <NavText>Home</NavText>
-                                </NavItem>
-                                <NavItem eventKey="devices">
-                                    <NavIcon>
-                                        <i
-                                            className="fa fa-fw fa-device"
-                                            style={{ fontSize: "1.75em" }}
-                                        />
-                                    </NavIcon>
-                                    <NavText>Devices</NavText>
-                                </NavItem>
-                            </SideNav.Nav>
-                        </SideNav>
-                    )}
-                />
                 <NavBar />
                 <Switch>
                     <Route
