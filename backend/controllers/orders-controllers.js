b68d4d39b9fbe689da6718b1f7694d4dc44c1d86
Wei-Chin, Chien <r08921a26@ntu.edu.tw>
2021-01-20 04:02:22 +0800

    add order return name

--- backend/controllers/orders-controllers.js
+++ backend/controllers/orders-controllers.js
@@ -77,7 +77,10 @@ const getOrderByUid = async (req, res, next) => {
 
     let userOrders;
     try {
-        userOrders = await Order.find({ creator: userId });
+        userOrders = await Order.find({ creator: userId }).populate({
+            path: "creator",
+            select: "name",
+        });
         console.log(userOrders);
     } catch (error) {
         const err = new HttpError(
