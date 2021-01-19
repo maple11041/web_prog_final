6f885dd9e3a4dd59a739cdc29c24573f14031af2
j20214369 <j20214369@gmail.com>
2021-01-19 14:45:23 +0800

    可以參考history的部分

---
+++ frontend/src/components/Orders2.js
@@ -0,0 +1,170 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import Collapse from "@material-ui/core/Collapse";
+import IconButton from "@material-ui/core/IconButton";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
+
+const useRowStyles = makeStyles({
+  root: {
+    "& > *": {
+      borderBottom: "unset"
+    }
+  }
+});
+
+function createData(name, price, paid) {
+  return {
+    name,
+    price,
+    paid,
+    item:['first','second'],
+    amount:[5,6],
+    history: [
+      { orderId: "2020-01-05", customerId: ["11091700","222a","999"], amount: [3,7,80 ],price:77,paid:true,canceled:false},
+      { orderId: "2020-01-02", customerId: ["Anonymous"], amount:[ 1 ],price:99,paid:false,canceled:false}
+    ]
+  };
+}
+
+function Row(props) {
+  const { row } = props;
+  const [open, setOpen] = React.useState(false);
+  const classes = useRowStyles();
+
+  return (
+    <React.Fragment>
+      <TableRow className={classes.root}>
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {row.name}
+        </TableCell>
+        <TableCell>
+        {row.item.map((item)=>(<div>{item}
+        {console.log(item)}</div>))}
+        </TableCell>
+        <TableCell>
+        {row.amount.map((item)=>(<div>{item}
+        {console.log(item)}</div>))}
+        </TableCell>
+        <TableCell align="right">{row.price}</TableCell>
+        <TableCell align="right">{row.paid}</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box margin={1}>
+              <Typography variant="h6" gutterBottom component="div">
+                訂單
+              </Typography>
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>訂單編號</TableCell>
+                    <TableCell>商品</TableCell>
+                    <TableCell align="right">數量</TableCell>
+                    <TableCell align="right">總價</TableCell>
+                    <TableCell align="center">付款狀態</TableCell>
+                    <TableCell>取消訂單</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {row.history.map((historyRow) => (
+                    <TableRow key={historyRow.orderId}>
+                      <TableCell component="th" scope="row">
+                        {historyRow.orderId}
+                      </TableCell>
+                      <TableCell>
+                      {historyRow.customerId.map((item)=>(<div>{item}
+                      {console.log(item)}</div>))}
+                      </TableCell>
+                      <TableCell align = "right">
+                      {historyRow.amount.map((item)=>(<div>{item}
+                      {console.log(item)}</div>))}
+                      </TableCell>
+                      <TableCell align="right">
+                        {historyRow.price}
+                      </TableCell>
+                      <TableCell align="center">
+                        {<input type = 'checkbox'/>}
+                      </TableCell>
+                      <TableCell align="right">
+                        {<input type = 'checkbox'/>}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+}
+/*
+Row.propTypes = {
+  row: PropTypes.shape({
+    price: PropTypes.number.isRequired,
+    paid: PropTypes.number.isRequired,
+    
+    history: PropTypes.arrayOf(
+      PropTypes.shape({
+        amount: PropTypes.number.isRequired,
+        customerId: PropTypes.string.isRequired,
+        date: PropTypes.string.isRequired
+      })
+    ).isRequired,
+    name: PropTypes.string.isRequired,
+  }).isRequired
+};
+*/
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0),
+  createData("Ice cream sandwich", 237, 9.0),
+  createData("Eclair", 262, 16.0),
+  createData("Cupcake", 305, 3.7),
+  createData("Gingerbread", 356, 16.0)
+];
+
+export default function CollapsibleTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="collapsible table">
+        <TableHead>
+          <TableRow>
+            <TableCell />
+            <TableCell>訂購人</TableCell>
+            <TableCell>商品</TableCell>
+            <TableCell>數量</TableCell>
+            <TableCell align="right">總金額</TableCell>
+            <TableCell align="right">付款狀態</TableCell>            
+            <TableCell align="right"></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <Row key={row.name} row={row} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
